bd85250723323683f55f2c47dae9a153e10c0463
mohamdnur2 <44902522+mohamdnur2@users.noreply.github.com>
2018-11-12 11:56:33 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Eris = require("eris");
+var x5bzteam = new Eris("NDAxMDE2MDEyNDA2OTE1MDcz.DsoFIA.O9JPi2SQ3xMVFH53iG2Iq4ZUQEU");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
+x5bz = "510490791748173824";
+nick = "( .في المدرسة (مشغول";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
